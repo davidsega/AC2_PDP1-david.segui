df65d69c5a1908759e533d81c737cd51c5748e4f
MSI-David <david.segui@students.salle.url.edu>
2018-12-22 19:54:57 +0100

    feat: creamos la carpeta test con el proposito de testear los diferentes modulos por separado y luego en conjunto.
    Anyadimos el fichero testD.c que usaremos para debuggar el modulo
    Destination

---
+++ test/testD.c
@@ -0,0 +1,14 @@
+#include "../model/destination/destination.h"
+#include "../logic/utils/utils.h"
+#include <stdlib.h>
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+#define PATH  "../data/mainDatabase.nymal"
+
+
+void main(char** args){
+
+}
+
+
